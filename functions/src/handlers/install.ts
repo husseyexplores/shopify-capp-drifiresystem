cecdd213447b613d78def80a5c81f6b790d70cf4
husseyexplores <husseyexplores@gmail.com>
2023-08-29 23:26:49 +0500

    Update install handler

--- functions/src/handlers/install.ts
+++ functions/src/handlers/install.ts
@@ -1,11 +1,12 @@
 import * as logger from "firebase-functions/logger";
 import * as webhookQ from "../lib/graphql/webhooks";
 import * as shopQ from "../lib/graphql/shop";
+import type { WebhookSubscription } from "../lib/graphql/types/webhook";
 import type { RequestHandler } from "../types";
 import { C } from "../lib/firebase-admin";
 import { html } from "../lib/html/install";
 
-const REQUIRED_WEBHOOKS: Parameters<
+const AVAILABLE_WEBHOOKS: Parameters<
   (typeof webhookQ)["mutation"]["create"]
 >[0]["variables"][] = [
   {
@@ -25,26 +26,43 @@ const REQUIRED_WEBHOOKS: Parameters<
   //   },
   // },
 ];
+type WebhookCreateInput = (typeof AVAILABLE_WEBHOOKS)[number];
+
+const AVAILABLE_WH_PUBSUB_TOPICS = AVAILABLE_WEBHOOKS.map(
+  (x) => x.input.pubSubTopic
+);
 
 type HandlerOptions = {
   payload?: never; // fulfillment or order object
   auth: { shop: string; accessToken: string };
 };
 
-export async function handler({ auth }: HandlerOptions) {
+// Add webhooks
+async function registerWebhooks(
+  auth: HandlerOptions["auth"],
+  predicate?: (webhookCreateInput: WebhookCreateInput) => boolean
+) {
   logger.debug(`[shop::${auth.shop}] Registering webhooks handler triggered`);
 
   let webhooksList = await getAllWebhooks(auth);
-  let numWebhooksToInstall = webhooksList.missing.length;
+
+  // predicate is required!
+  let webhooksToInstall = predicate
+    ? webhooksList.missing.filter(predicate)
+    : [];
+  let numWebhooksToInstall = webhooksToInstall.length;
 
   // Install missing webhooks
-  if (webhooksList.missing.length > 0) {
+  if (webhooksToInstall.length > 0) {
     logger.debug(
-      `[shop::${auth.shop}] Registering ${numWebhooksToInstall} webhooks`
+      `[shop::${auth.shop}] Registering ${numWebhooksToInstall} webhooks`,
+      {
+        pubsubTopics: webhooksToInstall.map((x) => x.input.pubSubTopic),
+      }
     );
 
     await Promise.all(
-      webhooksList.missing.map((wh) =>
+      webhooksToInstall.map((wh) =>
         webhookQ.mutation
           .create({
             variables: wh,
@@ -74,19 +92,26 @@ export async function handler({ auth }: HandlerOptions) {
 
   logger.info(`[shop::${auth.shop}] Installed webhooks are `, {
     justInstalled: numWebhooksToInstall,
-    list: webhooksList.installed,
+    installed: webhooksList.installed,
+    missing: webhooksList.missing,
   });
 
   return webhooksList;
 }
 
-export async function deleteAll(auth: HandlerOptions["auth"]) {
+async function deleteWebhooks(
+  auth: HandlerOptions["auth"],
+  predicate?: (wh: WebhookSubscription) => boolean
+) {
   logger.warn(`[shop::${auth.shop}] Deleting all webhooks...`);
 
   const webhooksList = await getAllWebhooks(auth);
+  const installedWebhooks = predicate
+    ? webhooksList.installed.filter(predicate)
+    : webhooksList.installed;
 
   await Promise.all(
-    webhooksList.installed.map((wh) => {
+    installedWebhooks.map((wh) => {
       return webhookQ.mutation.delete({ auth, variables: { id: wh.id } });
     })
   );
@@ -95,12 +120,19 @@ export async function deleteAll(auth: HandlerOptions["auth"]) {
   return true;
 }
 
-async function getAllWebhooks(auth: HandlerOptions["auth"]) {
+type WebhooksOverview = {
+  available: string[];
+  installed: WebhookSubscription[];
+  missing: WebhookCreateInput[];
+};
+async function getAllWebhooks(
+  auth: HandlerOptions["auth"]
+): Promise<WebhooksOverview> {
   const registedWebhooks = await webhookQ.query.listRegisteredWebhooks({
     auth,
   });
 
-  const missingWebhooks = REQUIRED_WEBHOOKS.filter((requiredWh) => {
+  const missingWebhooks = AVAILABLE_WEBHOOKS.filter((requiredWh) => {
     const insalled = registedWebhooks.find(
       (regWh) =>
         regWh.topic === requiredWh.topic &&
@@ -110,7 +142,28 @@ async function getAllWebhooks(auth: HandlerOptions["auth"]) {
     return !insalled;
   });
 
-  return { installed: registedWebhooks, missing: missingWebhooks };
+  return {
+    available: AVAILABLE_WH_PUBSUB_TOPICS,
+    installed: registedWebhooks,
+    missing: missingWebhooks,
+  };
+}
+
+function getWebhooksToConfigure(x: unknown) {
+  let matchedWebhooks: null | string[] = null;
+
+  if (x === "ALL") {
+    matchedWebhooks = AVAILABLE_WH_PUBSUB_TOPICS;
+  } else if (Array.isArray(x)) {
+    matchedWebhooks = x.filter(
+      (x) =>
+        x &&
+        typeof x === "string" &&
+        AVAILABLE_WEBHOOKS.find((y) => y.input.pubSubTopic === x)
+    );
+  }
+
+  return matchedWebhooks;
 }
 
 /*
@@ -118,7 +171,8 @@ async function getAllWebhooks(auth: HandlerOptions["auth"]) {
   ----------------------
 
   ```js
-  fetch(`https://install-blablabla.app`, {
+  const installUrl = "https://install-blablabla.app";
+  fetch(installUrl, {
     method: "POST",
     headers: {
       'content-type': 'application/json'
@@ -133,8 +187,28 @@ async function getAllWebhooks(auth: HandlerOptions["auth"]) {
   })
   .then(r => r.json())
   .then(console.log)
+
+  fetch(installUrl, {
+    method: "POST",
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      method: 'PUT',
+      shop: 'handle.myshopify.com',
+      accessToken: '<shopify access token>',
+      webhooks: {
+        add: ["update_payment_terms"]
+      }
+    })
+  })
+  .then(r => r.json())
+  .then(console.log)
+
   ```
  */
+
+const _METHODS = ["GET", "POST", "DELETE", "PUT"] as const;
 
 export const onRequest: RequestHandler = async function onRequest(
   request,
@@ -153,37 +227,62 @@ export const onRequest: RequestHandler = async function onRequest(
     return;
   }
 
-  const { shop, method } = request.body;
+  const _request_invalid_params: Record<string, string> = {};
+
+  let { shop, method: _method } = request.body;
+  if (!shop || typeof shop !== "string") {
+    _request_invalid_params.shop = "Please provide a shop handle";
+  }
+
+  if (
+    !_method ||
+    typeof _method !== "string" ||
+    !_METHODS.includes(_method.toUpperCase() as any)
+  ) {
+    _request_invalid_params.method = `Please provide a valid method. (${_METHODS.join(
+      ", "
+    )})`;
+  }
+
+  const method = _method.toUpperCase() as (typeof _METHODS)[number];
   let bodyAccessToken =
     typeof request.body.accessToken === "string"
       ? (request.body.accessToken as string)
       : null;
 
-  if (
-    !shop ||
-    !method ||
-    typeof shop !== "string" ||
-    typeof method !== "string"
-  ) {
+  if (method !== "GET") {
+    if (!bodyAccessToken) {
+      _request_invalid_params.accessToken = "Please provide access token";
+    }
+  }
+
+  if (Object.keys(_request_invalid_params).length > 0) {
     response.status(400).json({
       error: {
         title: "Bad request.",
         message: "Missing required params",
-        params: { shop, method, accessToken: bodyAccessToken },
+        params: _request_invalid_params,
       },
     });
     return;
   }
 
-  // User must provide an `accessToken` for sensitive operations
-  if (method !== "GET") {
-    if (!bodyAccessToken || typeof bodyAccessToken !== "string") {
-      response.status(400).json({
+  // validate token
+  if (bodyAccessToken) {
+    const shopInfo = await shopQ.query
+      .info({
+        auth: { shop, accessToken: bodyAccessToken },
+      })
+      .catch((e) => null);
+
+    if (!shopInfo || shopInfo.myshopifyDomain !== shop) {
+      response.status(401).json({
         error: {
-          title: "Missing required params",
-          message: "Please provide an `accessToken` (string) in the body.",
+          title: "Unable to fetch shop info.",
+          message: "Bad access token",
+          tip: "If you're sure that the access token is 100% correct, then please wait a couple of minutes and then try again.",
         },
-        params: { accessToken: bodyAccessToken },
+        shopInfo,
       });
       return;
     }
@@ -218,21 +317,6 @@ export const onRequest: RequestHandler = async function onRequest(
     }
     auth.accessToken = bodyAccessToken;
 
-    const shopInfo = await shopQ.query.info({ auth }).catch(() => null);
-
-    if (!shopInfo || shopInfo.myshopifyDomain !== shop) {
-      response.status(400).json({
-        error: {
-          title: "Unable to fetch shop info.",
-          message: "Bad access token",
-          tip: "If you're sure that the access token is 100% correct, then please wait a couple of minutes and then try again.",
-        },
-        shopInfo,
-        auth,
-      });
-      return;
-    }
-
     // access token is correct. Store the `accessToken` in db
     await shopRef.ref.set(auth);
   }
@@ -240,34 +324,72 @@ export const onRequest: RequestHandler = async function onRequest(
   try {
     // Register webhooks
     if (method === "POST") {
+      // new app?
+      // delete the previous one
       if (storedAuth && storedAuth.accessToken !== auth.accessToken) {
-        await deleteAll(storedAuth);
+        await deleteWebhooks(storedAuth);
       }
 
-      const data = await handler({ auth });
+      const toAdd = getWebhooksToConfigure(request.body.webhooks?.add);
+
+      const webhooks = await (toAdd
+        ? registerWebhooks(auth, (whInput) =>
+            toAdd.includes(whInput.input.pubSubTopic)
+          )
+        : getAllWebhooks(auth));
 
       response.status(200).json({
         success: true,
-        registered_webhooks: data,
+        data: { webhooks },
       });
       return;
     }
 
     // Delete all webhooks
     if (method === "DELETE") {
-      await Promise.all([deleteAll(auth), shopRef.ref.delete()]);
+      await Promise.all([deleteWebhooks(auth), shopRef.ref.delete()]);
 
-      response
-        .status(200)
-        .json({ success: true, message: "All webhooks deleted" });
+      response.status(200).json({
+        success: true,
+        message: "Shop uninstalled. All webhooks deleted",
+      });
       return;
     }
 
     if (method === "GET") {
-      const list = await getAllWebhooks(auth);
+      const webhooks = await getAllWebhooks(auth);
       response.status(200).json({
         success: true,
-        list,
+        data: { webhooks },
+      });
+      return;
+    }
+
+    if (method === "PUT") {
+      const toAdd = getWebhooksToConfigure(request.body.webhooks?.add);
+      const toRemove = getWebhooksToConfigure(request.body.webhooks?.remove);
+
+      let webhooks: WebhooksOverview | null = null;
+
+      if (toRemove) {
+        await deleteWebhooks(auth, (wh) =>
+          toRemove.includes(wh.endpoint.pubSubTopic)
+        );
+      }
+
+      if (toAdd) {
+        webhooks = await registerWebhooks(auth, (whInput) =>
+          toAdd.includes(whInput.input.pubSubTopic)
+        );
+      }
+
+      if (!webhooks) {
+        webhooks = await getAllWebhooks(auth);
+      }
+
+      response.status(200).json({
+        success: true,
+        data: { webhooks },
       });
       return;
     }
